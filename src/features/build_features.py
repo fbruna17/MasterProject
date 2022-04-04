8f5a6b9a9f42a20ac31ce241c1ab3a869c55d268
fbruna17 <36191117+fbruna17@users.noreply.github.com>
2022-04-04 11:34:35 +0200

    small fix (very good description, it is)

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -13,7 +13,6 @@ def encode_cycle(df, columns):
     for col in columns:
         df[f"{col}_sin"] = sin(2 * pi * df[col] / df[col].max())
         df[f"{col}_cos"] = cos(2 * pi * df[col] / df[col].max())
-        df.drop(columns=col, inplace=True)
     return df
 
 
@@ -21,7 +20,7 @@ def build_features(df):
     lead_feature = ['Azimuth', 'Zenith']
     cyclic_features = ['Hour', 'Year', 'Day', 'WindDir', 'Zenith', 'Azimuth']
     number_of_leads = 4
-
-    df = generate_leads(df, lead_feature, number_of_leads=number_of_leads)
     df = encode_cycle(df, cyclic_features)
+    df = generate_leads(df, lead_feature, number_of_leads=number_of_leads)
+    df = df.drop(columns=cyclic_features)
     return df
